a5b61cdf934c56812abc093424dbb25f3fd57010
11SID22 <sid111097@gmail.com>
2024-08-23 13:47:09 +0530

    Created required components & built the Header & cart in it.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import classes from './Header.module.css';
+import mealsImage from '../../assets/image.jpg';
+import HeaderCardButton from "./HeaderCartButton";
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>FridayMeals</h1>
+                <HeaderCardButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="A table full of delicious food" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
